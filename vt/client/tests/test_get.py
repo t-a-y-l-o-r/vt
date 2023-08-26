528f897fd88d740e62826d1d0c8f8cc434faadfc
t-a-y-l-o-r <taylorjcochran@hotmail.com>
2023-08-26 08:23:49 -0700

    Build first test :)

--- vt/client/tests/test_get.py
+++ vt/client/tests/test_get.py
@@ -1,1 +1,19 @@
 from vt.client import Client
+from vt.env import Env
+import pytest
+
+
+@pytest.fixture
+def client():
+    env = Env()
+    client = Client(env.url, env.key)
+    return client
+
+
+@pytest.mark.vcr()
+def test_googl_ip(client):
+    ip = "8.8.8.8"
+    response = client.get(f"/ip_addresses/{ip}")
+    # if this fails for any reason then the test fails
+    data = response.json()
+    assert response.status_code == 200, data
